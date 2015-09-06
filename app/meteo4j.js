23f2be796e8ac1df718584bbf61beffd1a0ebc3d
James Newton <bitbucket@lexogram.com>
2015-09-06 12:18:08 -0400

    Add A Door shows available entranc
    Add first step to "Add a door"
    
    - At least two rooms must exist before the Add a Door widget shows
    - Only rooms that are not connected to all other rooms are shown in the Master popup menu
    - Only rooms that aren't already connected to the current master room are shown
    - Checkbox to indicate if door should be locked
    - If the door should be locked, a popup appears to show all possible rooms in which to hide it.
    - TODO:
        - The Add Door button is currently inactive)
        - The door-with-the-key is not remembered if you toggle the locked state off and then on again

--- app/meteo4j.js
+++ app/meteo4j.js
@@ -2,15 +2,27 @@
 var queries = {
   "allRooms": {
     collection: Meteor.neo4j.collection("Rooms")
-  , query: "MATCH (room:Room) RETURN room"
+  , query: "MATCH (room:Room) RETURN room ORDER BY room.name"
   , link: "room"
   }
 , "roomsWithDoors": {
     collection: Meteor.neo4j.collection("RoomsWithDoors")
   , query:
       "MATCH (entrance:Room)-[door:DOOR]->(exit:Room) " +
-      "RETURN exit;" 
+      "RETURN exit " +
+      "ORDER BY entrance.name" 
   , link: "exit"
+  }
+, "roomThatCouldUseADoor": {
+    collection: Meteor.neo4j.collection("RoomThatCouldUseADoor")
+  , query: 
+      "MATCH (entrance:Room), (exit:Room) " +
+      "WHERE entrance <> exit " +
+      "AND NOT (entrance)-[:DOOR]->(exit) " +
+      "RETURN DISTINCT entrance " +
+      "ORDER BY entrance.name " +
+      "LIMIT 1"
+  , link: "entrance"
   }
 , "roomsForNewDoors": {
     collection: Meteor.neo4j.collection("RoomsForNewDoors")
@@ -18,18 +30,20 @@ var queries = {
      "MATCH (entrance:Room), (exit:Room) " +
      "WHERE NOT (entrance)-[:DOOR]->(exit) " +
      "AND entrance <> exit " +
-     "RETURN entrance" // "RETURN DISTINCT entrance" fails silently
+     "RETURN entrance " + // "RETURN DISTINCT entrance" fails silently
+     "ORDER BY entrance.name" 
   , link: "entrance"
   }
 , "newDoorsForRoom": {
     collection: Meteor.neo4j.collection("NewDoorsForRoom")
   , query: 
       "MATCH (entrance:Room), (exit:Room) " +
-      "WHERE entrance.name = {name} " +
+      "WHERE entrance.name = {fromRoomId} " +
       "AND NOT (entrance)-[:DOOR]->(exit) " +
       "AND entrance <> exit " +
-      "RETURN exit"
-  , options: {name: "Room A"}
+      "RETURN exit " +
+      "ORDER BY exit.name" 
+  , options: {fromRoomId: "Room A"}
   , link: "exit"
   }
 }
@@ -59,6 +73,9 @@ if (Meteor.isServer) {
 }
 
 if (Meteor.isClient) {
+
+  Session.set("fromRoomId", "")
+
   Tracker.autorun(function createSubscriptions(){
     var queryKeys = Object.keys(queries)
     queryKeys.forEach(subscribe)
@@ -68,11 +85,7 @@ if (Meteor.isClient) {
       var options = queryData.options
       var link = queryData.link
       var collection = queryData.collection
-      console.log(key, options, link)
       var subscription = collection.subscribe(key, options, link)
-    
-      console.log(key + ": ", collection)
-      console.log("subscription: ", subscription)
     }
   })
 
@@ -117,94 +130,136 @@ if (Meteor.isClient) {
     }
   })
 
-  Template.addDoor.events({
-    'click #addDoor': function () {
-      var $from = $('#fromRoomName')
-      var $to = $('#toRoomName')
-      var $locked = $('#locked')
-      var $key = $('#keyRoomName')
+  Template.addDoor.onRendered (function () {
+    var fromRoomId = $("#fromRoomName :selected").text()
+    Session.set("fromRoomId", fromRoomId)
+  })
 
-      var fromName = $from.val()
-      var toName = $to.val()
-      var keyName = $locked.checked ? $key.val() : null
+  Template.addDoor.events(
+    { 'click #addDoor': function () {
+        var $from = $('#fromRoomName')
+        var $to = $('#toRoomName')
+        var $locked = $('#locked')
+        var $key = $('#keyRoomName')
 
-      var query = 
-      "MATCH (from:Room), (to:Room)" +
-      " WHERE from.name = '" + fromName + "'" +
-      " AND to.name = '" + toName + "'" +
-      " CREATE (from)-[door:DOOR"
+        var fromName = $from.val()
+        var toName = $to.val()
+        var keyName = $locked.checked ? $key.val() : null
 
-      if ($locked.attr("checked")) {
-        query += " {lock: '" + toName+ "'}"
-      }
-      
-      query += "]->(to)" +
-      " RETURN from, door, to"
+        var query = 
+        "MATCH (from:Room), (to:Room)" +
+        " WHERE from.name = '" + fromName + "'" +
+        " AND to.name = '" + toName + "'" +
+        " CREATE (from)-[door:DOOR"
 
-      if(fromName){
-        if(toName){
-          Meteor.neo4j.query(query, null, doorAdded)
+        if ($locked.attr("checked")) {
+          query += " {lock: '" + toName+ "'}"
+        }
+        
+        query += "]->(to)" +
+        " RETURN from, door, to"
+
+        if(fromName){
+          if(toName){
+            Meteor.neo4j.query(query, null, doorAdded)
+          }
+        }
+
+        function doorAdded(error, data){
+          console.log("doorAdded: ", error, data)
+          // { door: [ {
+          //     lock: "Room 2"
+          //   , metadata: {
+          //       id: 7
+          //     , type: "DOOR"
+          //     }
+          //   , relation: {
+          //       end: "87"
+          //     , extensions: {}
+          //     , self: "7"
+          //     , start: "86"
+          //     , type: "DOOR"
+          //     }
+          //   } ]
+          // , [ { _id: "gaDMbc6KpmpGwyzTS"
+          //     , keys: []
+          //     , metadata: {
+          //         id: 86
+          //       , labels: ["Room"]
+          //       }
+          //     , name: "Room 1"
+          //     }]
+          // , [ { _id: ""d6TJWJ3jBtKMfYgLX""
+          //     , keys: []
+          //     , metadata: {
+          //         id: 86
+          //       , labels: ["Room"]
+          //       }
+          //     , name: "Room 2"
+          //     }]
+          // }
         }
       }
 
-      function doorAdded(error, data){
-        console.log("doorAdded: ", error, data)
-        // { door: [ {
-        //     lock: "Room 2"
-        //   , metadata: {
-        //       id: 7
-        //     , type: "DOOR"
-        //     }
-        //   , relation: {
-        //       end: "87"
-        //     , extensions: {}
-        //     , self: "7"
-        //     , start: "86"
-        //     , type: "DOOR"
-        //     }
-        //   } ]
-        // , [ { _id: "gaDMbc6KpmpGwyzTS"
-        //     , keys: []
-        //     , metadata: {
-        //         id: 86
-        //       , labels: ["Room"]
-        //       }
-        //     , name: "Room 1"
-        //     }]
-        // , [ { _id: ""d6TJWJ3jBtKMfYgLX""
-        //     , keys: []
-        //     , metadata: {
-        //         id: 86
-        //       , labels: ["Room"]
-        //       }
-        //     , name: "Room 2"
-        //     }]
-        // }
+    , 'change #fromRoomName': function () {
+        var fromRoomId = $("#fromRoomName :selected").text()
+        console.log("Change:", fromRoomId)
+        Session.set("fromRoomId", fromRoomId)
+      }
+
+    , 'change #locked': function (event) {
+        Session.set("locked", event.currentTarget.checked)
       }
     }
+  )
 
-  , 'change #fromRoomName': function () {
-      var fromRoomId = $("#fromRoomName :selected").text()
-      Session.set("fromRoomId", fromRoomId)
+  Template.addDoor.helpers(
+    { fromRooms: function() {
+        var results = getResults(queries.roomsForNewDoors)
+        if (!Session.get("fromRoomId").length) {
+          var fetched = results.fetch()
+          if (fetched.length > 0) {
+            Session.set("fromRoomId", fetched[0].name)
+          }
+        }
+        //console.log("From:",results.fetch())
+        return results
+      }
+
+    , toRooms: function () {
+        var key = "newDoorsForRoom"
+        var queryData = queries[key]
+        var options = { fromRoomId: Session.get("fromRoomId") }
+        var link = queryData.link
+        var collection = queryData.collection
+        // The options have changed, so subscribe() needs to be called
+        // again.
+        collection.subscribe(key, options, link)
+
+        var results = getResults(queryData)
+        //console.log("To:", results.fetch())
+        return results
+      }
+
+    , doorableRooms: function(){
+        var cursor = getResults(queries.roomsForNewDoors)
+        console.log("Doorable rooms")
+        return !!cursor.count()
+      }
+
+    , keyRooms: function(){
+        var cursor = getResults(queries.allRooms)
+        return cursor
+      }
+
+    , locked: function () {
+        var state = Session.get("locked") ? "locked" : "unlocked"
+        return state
+      }
+
+    , isLocked: function(){
+        return Session.get("locked")
+      }
     }
-  })
-
-  Template.addDoor.helpers({
-    fromRooms: function() {
-      var results = getResults(queries.roomsForNewDoors)
-      return results
-    }
-
-  , toRooms: function () {
-      var key = "newDoorsForRoom"
-      var queryData = queries[key]
-      var options = { name: Session.get("fromRoomId") }
-      var link = queryData.link
-      var collection = queryData.collection
-      collection.subscribe(key, options, link)
-
-      var results = getResults(queryData)
-      return results
-    }
-  })
+  )
 }
